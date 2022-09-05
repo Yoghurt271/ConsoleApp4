27b2e9ed15874635665424ce42e535f05b12533d
murvi <murvi@DESKTOP-9OTF2JL>
2022-09-05 22:46:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Водка, Пиво, Конец, Корпоратив";
+            char[] sep = { ' ' };
+            string[] parts = str.Split(sep);
+            int max = 0, ind = 0;
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (parts[i].Length > max)
+                {
+                    max = parts[i].Length;
+                    ind = i;
+                }
+            }
+            Console.WriteLine(parts[ind]);
+            Console.ReadLine();
+        }
+        
+    }
+}
